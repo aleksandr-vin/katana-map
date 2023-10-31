a029bd4fffed9625c9c6580a6fb5ea2d180005bc
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2023-10-31 10:41:20 +0100

    add maptiler
    See https://docs.maptiler.com/angular/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/map/map.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { Map, MapStyle, Marker, Popup, config } from '@maptiler/sdk';
+
+import { environment } from './../../environments/environment';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
+  map: Map | undefined;
+
+  @ViewChild('map')
+  private mapContainer!: ElementRef<HTMLElement>;
+
+  ngOnInit(): void {
+    config.apiKey = environment.maptilerApiKey;
+  }
+
+  ngAfterViewInit() {
+    const initialState = { lat: 34.577528, lng: 133.8223832, zoom: 14 };
+
+    this.map = new Map({
+      container: this.mapContainer.nativeElement,
+      style: MapStyle.STREETS,
+      center: [initialState.lng, initialState.lat],
+      zoom: initialState.zoom
+    });
+
+    let marker = new Marker({ color: "#FF0011" })
+      .setLngLat([133.8223832, 34.577528])
+      .addTo(this.map)
+      .setPopup(
+        new Popup()
+          .setLngLat([133.8223832, 34.577528])
+          .setHTML(`
+        <h3>Kurashiki Art Sword Museum</h3>
+        <a href="https://touken-sato.com" target="_blank">touken-sato.com</a>
+      `)
+      );
+  }
+
+  ngOnDestroy() {
+    this.map?.remove();
+  }
+
+
+}
